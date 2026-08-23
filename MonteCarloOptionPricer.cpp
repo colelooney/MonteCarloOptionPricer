b118cc6e52714321b3145b6fd94f7b706e86a835
Cole Looney <colelooney22@gmail.com>
2026-08-23 15:18:42 -0500

    write base pricing functions

---
+++ MonteCarloOptionPricer.cpp
@@ -0,0 +1,50 @@
+#include <random>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+class Asset{ // hold immutable market data
+    public: // will be used in other classes
+        Asset(double S0,double sigma):S0(S0),sigma(sigma){}
+        const double S0;
+        const double sigma;
+};
+
+double MonteCarloPrice(Asset asset, double r, double K, double T, int N){
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    std::normal_distribution<double> z_dist(0.0, 1.0);
+
+    double average_payoff = 0;
+    for (int i = 0; i < N; i++){
+        double Z = z_dist(gen);
+        double S_t = asset.S0 * exp((r - 0.5 * asset.sigma*asset.sigma)*T + asset.sigma*sqrt(T)*Z);
+        double payoff = std::max(S_t-K,0.0);
+        average_payoff += payoff;
+    }
+    average_payoff /= N;
+    double discounted_return = average_payoff * exp(-r*T);
+    return discounted_return;
+}
+
+double BlackScholesPrice(Asset asset, double r, double K, double T){
+    double d1= (log(asset.S0/K) +(r+0.5*asset.sigma*asset.sigma)*T)/(asset.sigma*sqrt(T));
+    double d2 = d1 - asset.sigma*sqrt(T);
+    double C = asset.S0*(0.5*(1+std::erf(d1/sqrt(2)))) - K * exp(-r*T) *0.5*(1+std::erf(d2/sqrt(2)));
+    return C;
+}
+
+int main(){
+    double S0 = 100;
+    double K = 100;
+    double r = 0.05;
+    double sigma = 0.2;
+    double T = 1;
+    int N = 100000;
+    Asset asset(S0,sigma);
+    
+    double monte_carlo_price = MonteCarloPrice(asset,r,K,T,N);
+    double closed_form_price = BlackScholesPrice(asset,r,K,T);
+    std::cout << "Monte Carlo Price " << monte_carlo_price << std::endl;
+    std::cout << "Closed form Price " << closed_form_price << std::endl;
+}
