297ee3ab7c1e9bb261597d659f87ba833c59bfad
Cole Looney <colelooney22@gmail.com>
2026-08-24 10:19:19 -0500

    remove defunct functions - integrated into class

--- MonteCarloOptionPricer.cpp
+++ MonteCarloOptionPricer.cpp
@@ -65,57 +65,6 @@ class MonteCarloEngine{ // Simulation Process
         double discountFactor(double T) const {return exp(-r*T);}
 };
 
-PricingResult MonteCarloPrice(Asset asset, double r, double K, double T, int N){
-    std::random_device rd;
-    std::mt19937_64 gen(rd());
-    std::normal_distribution<double> z_dist(0.0, 1.0);
-
-    double average_payoff = 0;
-    double average_payoff_squared = 0;
-    for (int i = 0; i < N; i++){
-        double Z = z_dist(gen);
-        double S_t = asset.S0 * exp((r - 0.5 * asset.sigma*asset.sigma)*T + asset.sigma*sqrt(T)*Z);
-        double payoff = std::max(S_t-K,0.0);
-        average_payoff += payoff;
-        double payoff_squared = payoff*payoff;
-        average_payoff_squared += payoff_squared;
-    }
-    average_payoff /= N;
-    average_payoff_squared /= N;
-    double price = average_payoff * exp(-r*T);
-    double variance = average_payoff_squared - average_payoff * average_payoff;
-    double standard_error = sqrt(variance) / sqrt(N);
-    double standard_error_discounted = standard_error * exp(-r*T);
-    return PricingResult(price,standard_error_discounted);
-}
-
-PricingResult MonteCarloPriceWithAntithetic(Asset asset, double r, double K, double T, int N){
-    std::random_device rd;
-    std::mt19937_64 gen(rd());
-    std::normal_distribution<double> z_dist(0.0, 1.0);
-
-    double average_payoff = 0;
-    double average_payoff_squared = 0;
-    for (int i = 0; i < N; i++){
-        double Z = z_dist(gen);
-        double S_t = asset.S0 * exp((r - 0.5 * asset.sigma*asset.sigma)*T + asset.sigma*sqrt(T)*Z);
-        double payoff_one = std::max(S_t-K,0.0);
-        average_payoff += payoff_one;
-        // compute antithetic
-        S_t = asset.S0 * exp((r - 0.5 * asset.sigma*asset.sigma)*T - asset.sigma*sqrt(T)*Z);
-        double payoff_two = std::max(S_t-K,0.0);
-        average_payoff += payoff_two;
-        average_payoff_squared += (payoff_one+payoff_two)*(payoff_one+payoff_two)/2; //payoffs are not independent
-    }
-    average_payoff /= 2*N;
-    average_payoff_squared /= 2*N;
-    double price = average_payoff * exp(-r*T);
-    double variance = average_payoff_squared - average_payoff * average_payoff;
-    double standard_error = sqrt(variance) / sqrt(N);
-    double standard_error_discounted = standard_error * exp(-r*T);
-    return PricingResult(price,standard_error_discounted);
-}
-
 double BlackScholesPrice(Asset asset, double r, double K, double T){
     double d1= (log(asset.S0/K) +(r+0.5*asset.sigma*asset.sigma)*T)/(asset.sigma*sqrt(T));
     double d2 = d1 - asset.sigma*sqrt(T);
